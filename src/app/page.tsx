2283c24f4a857c83cba93ee0db27ab3cc6cbec1b
MaevaWolff <maeva.wolff@hetic.net>
2023-04-11 17:26:04 +0200

    chore: add QueryClientProvider wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hi</h1>
+    </main>
   );
 }
